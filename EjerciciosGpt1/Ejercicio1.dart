0b8aa1025b48e31f735b5e8d71445e06e06a8c09
LilAguh <aguhochoa@gmail.com>
2024-08-27 18:37:51 -0300

    Ejercicio edad

---
+++ EjerciciosGpt1/Ejercicio1.dart
@@ -0,0 +1,10 @@
+/*
+    Declara una variable entera edad e inicialízala con un valor 
+    de tu elección. Luego, imprime un mensaje que diga: 
+    "Tienes X años", donde X es el valor de la variable.   
+*/
+
+void main() {
+  int edad = 25;
+  print("Tienes " + edad.toString() + " años.");
+}
