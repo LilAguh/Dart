1155418e733943905853c628b2a87824c2951a06
LilAguh <aguhochoa@gmail.com>
2024-08-27 19:33:15 -0300

    calcular el area de un circulo

---
+++ EjerciciosGpt1/Ejercicio2.dart
@@ -0,0 +1,11 @@
+/*
+    Crea una variable de tipo double para almacenar el valor de PI (3.14159) 
+    y usa esta variable para calcular el área de un círculo 
+    con un radio de 5.0. Imprime el resultado.  
+*/
+void main() {
+  double pi = 3.14159;
+  double diametro = 10;
+  double radio = diametro / 2;
+  print(pi * (radio * radio));
+}
